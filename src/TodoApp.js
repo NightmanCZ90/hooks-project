ff613702cd5a4e28045d46b26e2f9170217da754
NightmanCZ90 <novak.jan90@gmail.com>
2020-09-23 23:08:56 +0200

    added TodosContext

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 import useTodoState from './hooks/useTodoState';
+import { TodosProvider } from './contexts/TodosContext';
 
 
 function TodoApp() {
@@ -32,13 +33,15 @@ function TodoApp() {
       </AppBar>
       <Grid container justify='center' style={{ marginTop: '1rem' }} >
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList 
-            todos={todos} 
-            removeTodo={removeTodo} 
-            toggleTodo={toggleTodo} 
-            editTodo={editTodo}
-          />
+          <TodosProvider >
+            <TodoForm addTodo={addTodo} />
+            <TodoList 
+              todos={todos} 
+              removeTodo={removeTodo} 
+              toggleTodo={toggleTodo} 
+              editTodo={editTodo}
+            />
+          </TodosProvider>
         </Grid>
       </Grid>
     </Paper>
---
+++ src/contexts/TodosContext.js
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react';
+import useTodoState from '../hooks/useTodoState';
+
+const defaultTodos = [
+  { id: 1, task: "Invest warp drive", completed: true },
+  { id: 2, task: "Build a starship", completed: false }
+];
+export const TodosContext = createContext();
+
+export function TodosProvider(props) {
+  const todosStuff = useTodoState(defaultTodos);
+  return (
+    <TodosContext.Provider value={todosStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  )
+}
+
